2b397667597d8f8f0ff0cc82c37e78dccb2890a5
Prodeep0312 <109973323+Prodeep0312@users.noreply.github.com>
2026-05-12 09:56:00 +0530

    comments added

--- Threads/13_atomic.cpp
+++ Threads/13_atomic.cpp
@@ -4,7 +4,7 @@
 
 std::atomic<int>counter=0;
 //int counter=0;
-
+//If we want to avoid using mutex for small operations , we use atomic variable which ensures only one thread operates on it at a time
 void run(){
     for(int i=1;i<=100000;i++){
         counter++;
